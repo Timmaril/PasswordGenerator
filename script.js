433318a54be3220adc6a7ee33cbd861f161be201
Tim macharia <timmaril3@gmail.com>
2022-10-30 23:24:12 +0300

    changes were made to index file and script file

--- script.js
+++ script.js
@@ -1,17 +1,92 @@
-const characterAmountRange = document.getElementById
-('characterAmountRange')
-const characterAmountNumber = document.getElementById
-('characterAmountNumber')
+//Dom elements
+const resultElement = document.getElementById('passwordDisplay')
+const lengthElement = document.getElementById('characterAmountNumber')
+const uppercaseElement = document.getElementById('includeUppercase')
+const numbersElement = document.getElementById('includeNumbers')
+const symbolsElement = document.getElementById('includeSymbols')
+const generateElement = document.getElementById('button')
+const rangeElement = document.getElementById('characterAmountRange')
 const form = document.getElementById('passwordGeneratorForm')
 
-characterAmountNumber.addEventListener('input', syncCharacterAmount)
-characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+const randomFunc = {
+   upper: getRandomUpper,
+   number: getRandomNumber,
+   symbol: getRandomSymbol
+}
+//event liste
 form.addEventListener('submit', (e)=>{
    e.preventDefault()
 })
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-function syncCharacterAmount(e){
+generateElement.addEventListener('click', ()=>{
+   const length = lengthElement.value
+   // console.log(length)
+   const hasUpper = uppercaseElement.checked
+   const hasNumber = numbersElement.checked
+   const hasSymbols = symbolsElement.checked
+
+   resultElement.innerText = generatePassword(
+      hasUpper,
+      hasNumber, 
+      hasSymbols, 
+      length)
+
+})
+//Generate password function
+function generatePassword(upper,number,symbol, length){
+  
+   let genaratedPassword = ''
+
+   const typesCount = upper + number + symbol
+   // console.log('typesCount:', typesCount)
+   const typesArr = [{upper},{symbol},{number}].filter
+   (item => Object.values(item)[0])
+   // console.log('typesArr:',typesArr)
+
+   if(typesCount === 0){
+      return ''
+   }
+   if(typesCount != upper){
+      getRandomLower()
+   }
+
+   for(let i =0; i< length; i+= typesCount){
+      typesArr.forEach(type => {
+         const funcName = Object.keys(type)[0]
+
+         genaratedPassword += randomFunc[funcName]()
+      })
+   }
+   const finalPassword = genaratedPassword.slice(0, length)
+   return finalPassword
+}
+
+
+
+//Generator functions
+ function getRandomLower(){
+   return String.fromCharCode(Math.floor(Math.random() * 26)+ 97)
+ }
+
+ function getRandomUpper(){
+   return String.fromCharCode(Math.floor(Math.random()*26) +65)
+ }
+ function getRandomNumber(){
+   return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+ }
+
+ function getRandomSymbol(){
+   const symbols ='!@#$%^&*(){}><,.'
+   return symbols[Math.floor(Math.random()* symbols.length)]
+ }
+
+
+
+ function syncCharacterAmount(e){
    const value = e.target.value
-   characterAmountNumber.value = value
-   characterAmountRange.value = value
+   lengthElement.value = value
+   rangeElement.value = value
 }
